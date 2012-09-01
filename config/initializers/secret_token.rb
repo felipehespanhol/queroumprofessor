c5178e365cda1c9907296dae7d513c7c7a0e91b7
Felipe Hespanhol <felipeghespanhol@gmail.com>
2012-09-01 13:58:55 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hespanhol::Application.config.secret_token = '90c968228f589988e69880fb5097857c1fd5b8d8f3fa56e16dd84826b1642667fc55e392e3bcc2a8c20d284568e39c3dedd5abc4412eebf1f3b41dac85cab510'
