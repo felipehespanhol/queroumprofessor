b053badbaad7779f73aadac7c7a02b9b4aade5ac
Felipe Hespanhol <felipeghespanhol@gmail.com>
2012-09-02 16:42:10 -0300

    Created validations to search form.

--- app/assets/javascripts/teachers.js.coffee
+++ app/assets/javascripts/teachers.js.coffee
@@ -12,5 +12,14 @@ $ ->
     source: "/cidades/find"
     select: (event, ui) -> $("#location_name").val(ui.item.name)
 
+  $("#search_teacher_form button").click (event) ->
+    if $("#speciality_name").val() == ""
+      message = "Escolha uma especialidade para o seu professor. No que ele é fera?"
+    else if $("#location_name").val() == ""
+      message = "Escolha uma cidade para o seu professor. Onde você mora?"
+    if message
+      $("#messages").html("<div class='alert alert-block'>#{message}</div>")
+      event.preventDefault()
+
       
 	
--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,15 +1,16 @@
 class Authorization < ActiveRecord::Base
-  attr_accessible :provider, :teacher_id, :uid
+  attr_accessible :provider, :teacher_id, :teacher, :uid
+
   belongs_to :teacher
   validates :provider, :uid, :presence => true
 
- def self.find_or_create(auth_hash)
-  unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-    teacher = Teacher.create :name => auth_hash["info"]["name"]
-    auth = create :teacher => teacher, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
-  end
+  def self.find_or_create(auth_hash)
+    unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+      teacher = Teacher.create :name => auth_hash["info"]["name"]
+      auth = create :teacher => teacher, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
+    end
 
-  auth
-end
+    auth
+   end
 
 end
