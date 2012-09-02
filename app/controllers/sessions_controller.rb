4d38ba4de467eba79196f941858ebabb9ba887c0
adriano <adriano@adriano.(none)>
2012-09-02 12:06:50 -0300

    login mais bonito

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,20 +8,19 @@ class SessionsController < ApplicationController
 
   @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
   if @authorization
-    render :text => "Welcome back #{@authorization.teacher.name}! You have already signed up."
-  else
-    #auth_hash object has => provider,uid,info,credentials,  extra
+    session[:teacher] = @authorization.teacher.name
+    #  render :text => "Welcome back #{@authorization.teacher.name}! You have already signed up."
+  else @authorization
+    #iauth_hash object has => provider,uid,info,credentials,  extra
     teacher = Teacher.new :name => auth_hash["info"]["name"]
     teacher.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
     teacher.save
 
     session[:teacher] = teacher.name
 
-    #render "teachers/home"
-    redirect_to  :controller => "teachers", :action => "home"
-    #render :text => "Hi #{teacher.name}! You've signed up."
   end
 
+    redirect_to  :controller => "teachers", :action => "home"
   end
 
   def failure
