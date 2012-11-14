5f13703455524ef12f1865c06327015d0623dc28
Felipe Hespanhol <felipeghespanhol@gmail.com>
2012-11-14 12:11:42 -0200

    Added omniauth credentials for production environment.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,14 @@
 #require 'omniauth'
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-  provider :twitter, '31W0dLZzOL64OlSkSIAVA', 'VA57S7fwuZAOfPT5Ilr1nm2qC0QLFI3Y8t6EF9luk'
-  provider :google_oauth2, '276672130584.apps.googleusercontent.com', '2XlQhOq6Vo8nTDedQUADiTKA'
+  if Rails.env == "development" #Adriano's accounts on localhost:3000
+    provider :twitter, '31W0dLZzOL64OlSkSIAVA', 'VA57S7fwuZAOfPT5Ilr1nm2qC0QLFI3Y8t6EF9luk'
+    provider :google_oauth2, '276672130584.apps.googleusercontent.com', '2XlQhOq6Vo8nTDedQUADiTKA'
+  elsif Rails.env == "production" #Felipe's accounts on queroumprofessor.com.br
+    provider :twitter, 'hxPAUXG0wfLTInnOp3PmtA', '38DqvWwJduVwSY2KYAaoBHrdvlC712mnJXTVAFNj6Y'
+    provider :google_oauth2, '717960285215.apps.googleusercontent.com', 'N6aFFL6LL-h43zTFj3_O9GiB'
+    proviver :facebook, '433960636667257', 'a32ce6494961c9d25478455a59d5e8ce'
+  end
   #provider :facebook, 'APP_ID', 'APP_SECRET'
   #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end
